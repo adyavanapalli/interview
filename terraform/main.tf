48e2764a4b1de9f809bec3de3faffa19bc5368d1
payerpathinterviewee <payerpathinterviewee@gmail.com>
2025-03-26 22:25:43 +0530

    vent changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,10 @@ resource "azurerm_resource_group" "rg1" {
   location = var.location
 }
 
+
+resource "azurerm_virtual_network" "example" {
+  name                = var.vnet_name
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+  address_space       = ["10.0.0.0/16"]
+}
